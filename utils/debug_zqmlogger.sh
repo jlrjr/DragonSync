07a128424804a478acc4dd69266695b2dcd68336
John Roberts <jroberts@capnhq.gov>
2025-10-18 19:36:28 -0400

    tests

---
+++ utils/debug_zqmlogger.sh
@@ -0,0 +1,2 @@
+/usr/bin/python3 /home/dragon/WarDragon/DragonSync/utils/zmq_logger_for_kml.py --zmq-host 127.0.0.1 --min-log-interval 10 --zmq-port 4224 --include-system-location --zmq-status-port 4225 --debug
+
